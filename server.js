593de9b92d37abc4f2c3255261b0f39aa0b20f82
aser nasr <asernasr@outlook.com>
2026-01-09 16:55:26 -0500

    Increase JSON payload limit

--- server.js
+++ server.js
@@ -241,7 +241,7 @@ setStopChecker((runId) => {
     return false;
 });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 const SESSION_TTL_SECONDS = 10 * 365 * 24 * 60 * 60; // 10 years
 
 app.use(session({
